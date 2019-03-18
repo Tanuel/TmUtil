67edb8cda35f83bd9023f5068ea73db17dd0871c
Tanuel <tanuel.mategi@gmail.com>
2019-03-18 23:27:00 +0100

    Fix create method

--- src/util/create.ts
+++ src/util/create.ts
@@ -4,8 +4,19 @@
  * @param properties - properties to be merged into the element
  * @return HTMLElement
  */
-function create<T extends HTMLElement = HTMLElement>(name: string, properties: object = {}): T {
-        return {...document.createElement(name), ...properties} as T;
+function create<K extends keyof HTMLElementTagNameMap>(name: K, properties?: object): HTMLElementTagNameMap[K];
+function create(name: string, properties: object = {}): HTMLElement {
+    const el = document.createElement(name);
+    Object.entries(properties).forEach(([key, value]: [string, any]) => {
+        if (typeof value === "object") {
+            Object.entries(value).forEach(([nestedKey, nestedValue]) => {
+                el[key][nestedKey] = nestedValue;
+            });
+        } else {
+            el[key] = value;
+        }
+    });
+    return el;
 }
 
 export {create};
